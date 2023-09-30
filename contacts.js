761f04dce75d0a3f4828ea3683e5301c05de7a33
Nikita <barbos.nik007@gmail.com>
2023-10-01 00:35:21 +0200

    id change

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 import { promises as fs } from 'fs';
 import { fileURLToPath } from 'url';
+import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -56,22 +57,10 @@ async function removeContact(contactId) {
       }
 }
 
-function generateRandomId(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let randomId = '';
-  
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      randomId += characters.charAt(randomIndex);
-    }
-  
-    return randomId;
-  }
-
 async function addContact(name='Nugget', email='nuggetmail@gmail.com', phone='+380777444333') {
     try {
         const newContact = {
-            "id": generateRandomId(21),
+            "id": uuidv4(),
             "name": name,
             "email": email,
             "phone": phone
